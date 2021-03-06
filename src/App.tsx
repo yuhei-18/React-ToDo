d6449dc3666bd7490bec3518eec8e421cbf181e8
yuhei <kasaiyuhei8@gmail.com>
2021-03-07 02:00:00 +0900

    feat: LeftMenu コンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import LeftMenu from "components/organisms/LeftMenu";
 import './App.css';
-import api from 'api';
-import Card from 'components/atoms/Card';
 
 function App() {
-  const [todos, setTodos] = useState<Api.Todo[]>();
-
-  useEffect(() => {
-    api.todo.get().then((res) => {
-      setTodos(res);
-    })
-  }, [setTodos])
-
   return (
-    <div>
-      {todos?.map((todo) => (
-        <Card
-          title={todo.title}
-          priority={todo.priority}
-          is_done={todo.is_done}
-          due_date={todo.due_date}
-        />
-      ))}
-    </div>
-  );
+    <LeftMenu />
+  )
 }
 
 export default App;
