7469ae4ca0e67e6b457be11d5f9fb35bcfdd4e04
yuhei <kasaiyuhei8@gmail.com>
2021-03-09 00:52:38 +0900

    feat: router の設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import LeftMenu from "components/organisms/LeftMenu";
-import './App.css';
+import Top from "./components/pages";
+import './scss/_reset.module.scss';
+import styles from './styles.module.scss';
 
 function App() {
   return (
-    <LeftMenu />
+    <>
+      <div className={styles.App}>
+        <LeftMenu />
+
+        <div className={styles.Contents}>
+          <Switch>
+            <Route path="/" exact component={Top} />
+          </Switch>
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Top: React.FC = () => {
+  return(
+    <>
+      <h1>あいうえお</h1>
+    </>
+  )
+};
+
+export default Top;
