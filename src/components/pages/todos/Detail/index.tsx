9365177405e7e9ba45abb8c2afbb128c31a93729
yuhei <kasaiyuhei8@gmail.com>
2021-03-21 15:52:29 +0900

    feat: メニュー画面 コンテンツ画面 固定

--- src/components/pages/todos/Detail/index.tsx
+++ src/components/pages/todos/Detail/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import moment from 'moment';
+import clsx from "clsx";
 import styles from "components/pages/todos/Detail/styles.module.scss";
 
 interface PropsType {
@@ -8,7 +9,8 @@ interface PropsType {
 
 const Detail: React.FC<PropsType> = (props) => {
   const { todo } = props;
-  const priority = ["無", "低", "中", "高",];
+  const priority = ["無", "低", "中", "高"];
+  const now = moment();
 
   return(
     <div className={styles.todos}>
@@ -16,16 +18,26 @@ const Detail: React.FC<PropsType> = (props) => {
       <div className={styles.todo_info}>
         <p className={styles.priority}>
           優先度：
-          <strong className={styles.priority_value}>
+          <strong className={clsx({
+            [styles.priority_high]: todo?.priority === 3,
+            [styles.priority_medium]: todo?.priority === 2,
+            [styles.priority_low]: todo?.priority === 1,
+            [styles.priority_none]: todo?.priority === 0,
+          })}>
             {priority[todo?.priority ? todo?.priority : 0]}
           </strong>
         </p>
         <p className={styles.due_date}>
-          期限：{moment(todo?.due_date).format("LLL")}
+          期限：
+          <span className={clsx({
+            [styles.ago]: todo?.due_date && moment(todo?.due_date) < now
+          })}>
+            {moment(todo?.due_date).format("YYYY年 M月 DD日 dddd LT")}
+          </span>
         </p>
       </div>
       <p className={styles.updated_at}>
-        最終更新日：{moment(todo?.updated_at).format("LLL")}
+        最終更新日：{moment(todo?.updated_at).format("YYYY年 M月 DD日 dddd LT")}
       </p>
       <p>{todo?.content}</p>
     </div>
