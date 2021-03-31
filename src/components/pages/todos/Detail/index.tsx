dca8e058d6105f2c696e6f1f2611751956441017
yuhei <kasaiyuhei8@gmail.com>
2021-03-31 09:49:17 +0900

    feat: 詳細画面のアイコン追加

--- src/components/pages/todos/Detail/index.tsx
+++ src/components/pages/todos/Detail/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import moment from 'moment';
 import clsx from "clsx";
+import { MdUpdate, MdDateRange } from "react-icons/md"
+import { BsFillExclamationDiamondFill } from "react-icons/bs"
 import styles from "components/pages/todos/Detail/styles.module.scss";
 
 interface PropsType {
@@ -9,36 +11,37 @@ interface PropsType {
 
 const Detail: React.FC<PropsType> = (props) => {
   const { todo } = props;
-  const priority = ["無", "低", "中", "高"];
+  const priority = ["None", "Low", "Medium", "High"];
   const now = moment();
 
   return(
     <div className={styles.todos}>
       <h1 className={styles.title}>{todo?.title}</h1>
       <div className={styles.todo_info}>
-        <p className={styles.priority}>
-          優先度：
-          <strong className={clsx({
+        <div className={styles.priority}>
+          <BsFillExclamationDiamondFill className={styles.icon} />
+          <p className={clsx({
             [styles.priority_high]: todo?.priority === 3,
             [styles.priority_medium]: todo?.priority === 2,
             [styles.priority_low]: todo?.priority === 1,
             [styles.priority_none]: todo?.priority === 0,
           })}>
             {priority[todo?.priority ? todo?.priority : 0]}
-          </strong>
-        </p>
-        <p className={styles.due_date}>
-          期限：
-          <span className={clsx({
+          </p>
+        </div>
+        <div className={styles.due_date}>
+          <MdDateRange className={styles.icon} />
+          <p className={clsx({
             [styles.ago]: todo?.due_date && moment(todo?.due_date) < now
           })}>
             {moment(todo?.due_date).format("ll")}
-          </span>
-        </p>
+          </p>
+        </div>
+        <div className={styles.updated_at}>
+          <MdUpdate className={styles.icon} />
+          <p>updated {moment(todo?.updated_at).fromNow()}</p>
+        </div>
       </div>
-      <p className={styles.updated_at}>
-        updated {moment(todo?.updated_at).fromNow()}
-      </p>
       <p className={styles.content}>{todo?.content}</p>
     </div>
   )
