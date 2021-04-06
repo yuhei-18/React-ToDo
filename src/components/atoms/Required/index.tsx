cb6b58175a8f7f2ef8b7554fc1be4b1ef7d59bd9
yuhei <kasaiyuhei8@gmail.com>
2021-04-07 01:18:01 +0900

    feat: Required コンポーネントの作成

---
+++ src/components/atoms/Required/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './styles.module.scss';
+
+const Required: React.FC = () => {
+  return (
+    <span className={styles.required}>必須</span>
+  )
+}
+
+export default Required;
